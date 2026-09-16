bbde06864f9bae45adcdf3f7fdaf802fb3d5dfe2
NotBrian122 <s00254527@atu.ie>
2026-09-16 10:21:00 +0100

    this is the first major push to a simple exporting application

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, {Application, Request, Response} from "express" ; 
+const PORT = process.env.PORT || 3000; 
+
+const app: Application = express(); 
+
+ app.get("/ping", async (_req : Request, res: Response) => { 
+
+    res.json({ 
+    message: "We're so passing this module", 
+    }); 
+
+}); 
+
+ 
+
+app.listen(PORT, () => { 
+    console.log("Server is running on port", PORT); 
+    }); 
